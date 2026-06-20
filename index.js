727f64a9e97396e3ae1f5282299476718dab743f
bsen20 <officialbsen@gmail.com>
2026-06-20 11:21:09 +0530

    Pipeline for github action

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Hello, DevOps Pipelines!");
+console.log(
+  "This is a sample Node.js application for demonstrating DevOps pipelines with GitHub Actions.",
+);
---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("This is a test file for DevOps pipelines.");
+
+setTimeout(() => {
+  console.log("This simulates a longer-running process in the pipeline.");
+}, 3000);
+
+console.log("This will be logged immediately, before the timeout.");
+
+console.log("End of test file.");
